012a4333813483750486fb8c11e5ea8f4dd18e61
Recho77 <453934866@qq.com>
2017-03-25 16:45:42 +0800

    simplify the loop process, cut useless code

--- KeyboardRow.java
+++ KeyboardRow.java
@@ -7,10 +7,8 @@ import java.util.*;
 
 public class Solution {
     public String[] findWords(String[] words) {
-        //define 3 string for checking characters
-        String line1 = "qwertyuiop";
-        String line2 = "asdfghjkl";
-        String line3 = "zxcvbnm";
+       //define 3 string for checking characters
+        String[] line = {"qwertyuiop", "asdfghjkl", "zxcvbnm"};
         
         //defien a List to store the result value
         List<String> result = new ArrayList<String>();
@@ -19,42 +17,19 @@ public class Solution {
         String[] wordsLower = new String[words.length];
         for(int i = 0; i < words.length; i++) wordsLower[i] = words[i].toLowerCase();
         
-        for(int i = 0; i < words.length; i++){ //iterate the input string array
-            
-            //if first character in this string in line1
-            if (line1.contains(String.valueOf(wordsLower[i].charAt(0)))) {  //iterate this string, if all characters are in line1, append this string into result
-                boolean state = true;
-                for(int j = 1; j < wordsLower[i].length(); j++){
-                    if(!(line1.contains(String.valueOf(wordsLower[i].charAt(j))))) {
-                        state = false;
-                        break;
+        for (int k = 0; k < line.length; k++){
+            for(int i = 0; i < words.length; i++){ //iterate the input string array
+                  //iterate this string, if all characters are in line1, append this string into result
+                   if (line[k].contains(String.valueOf(wordsLower[i].charAt(0)))) {  
+                    boolean state = true;
+                    for(int j = 1; j < wordsLower[i].length(); j++){
+                        if(!(line[k].contains(String.valueOf(wordsLower[i].charAt(j))))) {
+                            state = false;
+                            break;
+                        }
                     }
-                }
                 if(state == true) result.add(words[i]);
-            }
-            
-            //if first character in this string in line2
-            if (line2.contains(String.valueOf(wordsLower[i].charAt(0)))) {  
-                boolean state = true;
-                for(int j = 1; j < wordsLower[i].length(); j++){
-                    if(!(line2.contains(String.valueOf(wordsLower[i].charAt(j))))) {
-                        state = false;
-                        break;
-                    }
                 }
-                if(state == true) result.add(words[i]);
-            }
-            
-              //if first character in this string in line3
-            if (line3.contains(String.valueOf(wordsLower[i].charAt(0)))) {  
-                boolean state = true;
-                for(int j = 1; j < wordsLower[i].length(); j++){
-                    if(!(line3.contains(String.valueOf(wordsLower[i].charAt(j))))) {
-                        state = false;
-                        break;
-                    }
-                }
-                if(state == true) result.add(words[i]);
             }
         }
         
