96f7a6a1a88f2b6c02e10b83c7b82efdb88db186
Recho77 <453934866@qq.com>
2017-03-26 16:10:48 +0800

    Update MaxConsecutiveOnes.java

--- MaxConsecutiveOnes.java
+++ MaxConsecutiveOnes.java
@@ -26,4 +26,11 @@ public class Solution {
         
         return len;
     }
+    
+       public int findMaxConsecutiveOnes_betterSolution(int[] nums) {
+        int maxHere = 0, max = 0;
+        for (int n : nums)
+            max = Math.max(max, maxHere = n == 0 ? 0 : maxHere + 1);
+        return max; 
+    } 
 }
